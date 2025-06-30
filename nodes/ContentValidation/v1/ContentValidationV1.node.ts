979cb6f25eca5b5bc272c91431691d3177da0f81
adameus03 <amadeusz.sitnicki2@gmail.com>
2025-06-30 14:05:00 +0200

    Basic support for html validation

--- nodes/ContentValidation/v1/ContentValidationV1.node.ts
+++ nodes/ContentValidation/v1/ContentValidationV1.node.ts
@@ -6,10 +6,15 @@ import {
 	//NodeConnectionType,
 	IExecuteFunctions,
 	//IHttpRequestOptions,
-	INodeTypeBaseDescription
+	INodeTypeBaseDescription,
+	NodeOperationError,
 } from 'n8n-workflow';
 
 import { versionDescription } from './actions/versionDescription';
+
+// TODO move to actions (like in Mattermost node: https://github.com/n8n-io/n8n/blob/master/packages/nodes-base/nodes/Mattermost/v1/actions/user/index.ts)
+import { HtmlValidate } from "html-validate";
+const htmlvalidate = new HtmlValidate();
 
 export class ContentValidationV1 implements INodeType {
 
@@ -32,10 +37,39 @@ export class ContentValidationV1 implements INodeType {
 
 		// Process each item
 		for (let i = 0; i < items.length; i++) {
+			let itemData: any = {};
+			switch (resource) {
+				case 'validator':
+					switch (operation) {
+						case 'Validate HTML': {
+							const html = this.getNodeParameter('html', i) as string;
+							// Validate HTML
+							const validationReport = await htmlvalidate.validateString(html);
+							itemData = {
+								report: validationReport
+							};
+							break;
+						}
+						case 'Validate spoken language': {
+							const text = this.getNodeParameter('text', i) as string;
+							// Placeholder for spoken language validation logic
+							// For now, we just return the text as is
+							//TODO replace with actual spoken language validation logic
+							itemData = { valid: true, text };
+							break;
+						}
+						default: {
+							throw new NodeOperationError(this.getNode(), `Operation "${operation}" is not supported for resource "${resource}"!`);
+						}
+					}
+					break;
+				default: {
+					throw new NodeOperationError(this.getNode(), `Resource "${resource}" is not supported!`);
+				}
+			}
 		  returnData.push({ json: {
-				i,
-				resource,
-				operation,
+				itemIndex: i,
+				...itemData
 			} });
 		}
 		// Map data to n8n data structure
